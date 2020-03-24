1b7e4a7abf3274c3ffe1553a0b13e54d3a5f6461
Adam <Adamsmith6300@gmail.com>
2020-03-23 18:50:47 -0700

    real first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express();
+const path = require('path');
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 5000
+
+
+app.use(express.static(path.join(__dirname,'public')));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+
+const expressHbs = require('express-handlebars');
+app.engine(
+    'hbs',
+    expressHbs({
+      layoutsDir: 'views/layouts/',
+      defaultLayout: 'main-layout',
+      extname: 'hbs'
+    })
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+
+app.get("/", (req,res)=>{
+  res.render("homepage",{});
+})
+
+
+app.listen(PORT, () => console.log('Server listening on port '+PORT))
+
+
+
---
+++ util/db.js
@@ -0,0 +1,10 @@
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'comp4711project',
+//     password: 'password'
+// });
+
+// module.exports = pool.promise();
