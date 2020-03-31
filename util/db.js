caeb8f4523c1f09ee52b4c8583f737c19eda88f7
Adam <Adamsmith6300@gmail.com>
2020-03-31 09:51:35 -0700

    setup cleardb for heroku

--- util/db.js
+++ util/db.js
@@ -1,5 +1,7 @@
 // const mysql = require('mysql2');
 
+//url:  mysql://bf02fe50141e00:863bb7b4@us-cdbr-iron-east-01.cleardb.net/heroku_11bab956933f1d3?reconnect=true
+
 // const pool = mysql.createPool({
 //     host: 'localhost',
 //     user: 'root',
