3e059100285175767fae3a811e838f986d7d3668
Adam <Adamsmith6300@gmail.com>
2020-04-07 12:31:58 -0700

    styling and toggle replies

--- public/js/usersHome.js
+++ public/js/usersHome.js
@@ -1,11 +1,20 @@
 
 
+let openRepliesId = null;
+
 
 const toggleViewReplies = id => {
+    if(openRepliesId != null && openRepliesId != id){
+      let previouslyOpen = document.querySelector(`#post-replies${openRepliesId}`);
+      previouslyOpen.style.display = "none";
+    }
+    
     let repliesDiv = document.querySelector(`#post-replies${id}`);
     if (repliesDiv.style.display === "none" || !repliesDiv.style.display) {
         repliesDiv.style.display = "block";
+        openRepliesId = id;
       } else {
         repliesDiv.style.display = "none";
-      }
+    }
+
 }
